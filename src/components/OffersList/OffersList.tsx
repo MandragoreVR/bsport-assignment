782d7c8cc05a7f9e72e9669a0488cbe1d549bbd6
Téo Chaillou <teo.chaillou@student-cs.fr>
2022-12-10 17:33:03 +0100

    Addition of Skeleton for RowCard loading

--- src/components/OffersList/OffersList.tsx
+++ src/components/OffersList/OffersList.tsx
@@ -4,12 +4,13 @@ import RowCard from "./RowCard";
 import { useQuery } from "react-query";
 import { useSearchParams } from "react-router-dom";
 import { getOffers } from "../../api";
+import RowCardSkeleton from "./RowCardSkeleton";
 
 const OffersList = () => {
   const [searchParams] = useSearchParams();
   const startDate = searchParams.get("startDate");
   const endDate = searchParams.get("endDate");
-  const { data: offers } = useQuery<FullOffer[]>("offers", () =>
+  const { isLoading, data: offers } = useQuery<FullOffer[]>("offers", () =>
     getOffers(
       startDate ?? new Date().toISOString().split("T")[0],
       1,
@@ -19,11 +20,20 @@ const OffersList = () => {
   return (
     <div className="flex flex-col border-2 border-red-600 w-[40vw] max-w-[575px] h-[85vh] ml-20 mt-5">
       <Filters />
-      <div className="overflow-auto mt-4">
-        {(offers ?? []).map((offer: FullOffer) => (
-          <RowCard key={offer.id} offer={offer} setSelectedOffer={() => {}} />
-        ))}
-      </div>
+      {isLoading ? (
+        <div className="mt-4">
+          <RowCardSkeleton />
+          <RowCardSkeleton />
+          <RowCardSkeleton />
+          <RowCardSkeleton />
+        </div>
+      ) : (
+        <div className="overflow-auto mt-4">
+          {(offers ?? []).map((offer: FullOffer) => (
+            <RowCard key={offer.id} offer={offer} setSelectedOffer={() => {}} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
