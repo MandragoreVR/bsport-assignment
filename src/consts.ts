2d470f5982d3edb8ba50e0e694254528a9d17dbf
Téo Chaillou <teo.chaillou@student-cs.fr>
2022-12-10 12:55:50 +0100

    Fetching of the data from the API

---
+++ src/consts.ts
@@ -0,0 +1,11 @@
+// The token is here for demo purposes only in order to make the app work with
+// as few efforts as possible from the user. It would of course need to be kept
+// in a .env in a production environment.
+export const authenticationToken: string =
+  "f18688960a8942c83d238b04e88389ac126bf55c";
+
+export const apiURL: string = "https://api.staging.bsport.io/api/v1";
+
+export const companyId: number = 6;
+
+export const pageSize: number = 20;
