6f1bd231437ece8fcfe4bf3a5aa4eb0351a36e25
Téo Chaillou <teo.chaillou@student-cs.fr>
2022-12-13 19:32:58 +0100

    Set up of the test for the Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ import React from "react";
 const Header = () => (
   <div className="flex flex-row justify-center items-center top-0 h-20 w-screen text-center border-b-2 border-solid border-b-gray-200">
     <img alt="logo" className="h-[3.5rem]" src="logo.png" />
-    <div className="hidden sm:flex text-[2.5rem] ml-7">
+    <div data-testid="header-text" className="hidden sm:flex text-[2.5rem] ml-7">
       Calendrier des activités
     </div>
   </div>
